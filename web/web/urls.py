f751269cd1ea4191b43ff9cbbc6bdb90161aa7d1
ForShure <evildevilzp@gmail.com>
2026-03-20 23:13:56 +0200

    fix docker-compose.yml

--- web/web/urls.py
+++ web/web/urls.py
@@ -12,6 +12,5 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.BASE_DIR / 'static')
