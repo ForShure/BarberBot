144ababa0c0cceed22b9f7560bf04745cd48eeed
ForShure <evildevilzp@gmail.com>
2026-04-11 09:31:51 +0300

    fix: google sheets debug

--- barberBot/services/google_sheets_service.py
+++ barberBot/services/google_sheets_service.py
@@ -1,22 +1,20 @@
 import gspread
 from asgiref.sync import sync_to_async
-import logging
 
 SHEET_NAME = "Вишенка"
 
 @sync_to_async
 def save_to_google_sheet(date, time, client_name, phone, service_name, master_name, price):
+    print("⏳ [GOOGLE] НАЧИНАЮ ЗАПИСЬ В ТАБЛИЦУ...")
     try:
-        # Авторизуемся через наш секретный файл
         gc = gspread.service_account(filename='google_credentials.json')
-        # Находим таблицу по имени и открываем первый лист
         sh = gc.open(SHEET_NAME)
         worksheet = sh.sheet1
 
-        # Формируем строку (список) с данными
         new_row = [str(date), str(time), client_name, phone, service_name, master_name, str(price)]
         worksheet.append_row(new_row)
+        print("✅ [GOOGLE] СТРОКА УСПЕШНО ДОБАВЛЕНА!")
         return True
     except Exception as e:
-        logging.error(f"Ошибка записи в Гугл Таблицу: {e}")
+        print(f"❌ [GOOGLE ОШИБКА]: {e}")
         return False
